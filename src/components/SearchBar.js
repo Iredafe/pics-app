e17d9ec3abb3ed41c75823f47c388e4dc0948617
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-06-03 16:25:46 -0600

    create event handler closes #5

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 class SearchBar extends React.Component{
 
+    onInputChange(event){
+        console.log(event.target.value)
+    }
+
 render(){
     return <div className="ui segment">
         Search Bar
         <br></br>
         <form className="ui form">
-        <input type='text' className="field"></input>
-        <button > Submit</button>
+        <input type='text' className="field" onChange={this.onInputChange}></input>
+        <button > Submit </button>
         </form>
     </div>
 }
