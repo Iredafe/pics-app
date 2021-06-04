3a98229e4282f266eabb73a6dc4d0bf9749132cc
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-06-03 22:06:16 -0600

    refactor to controlled elemnt closes #7

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 class SearchBar extends React.Component{
 
+    state = {term: ''};
+
 render(){
     return <div className="ui segment">
         Search Bar
         <br></br>
         <form className="ui form">
-        <input type='text' className="field" onChange={(e)=>{console.log(e.target.value)}}></input>
+        <input type='text' className="field" value={this.state.term} onChange={(e)=>this.setState({term:e.target.value})}></input>
         <button > Submit </button>
         </form>
     </div>
