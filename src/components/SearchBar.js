1e591131975a1d72fb363667fc9caa9ae9473415
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-06-04 17:35:28 -0600

    use controlled event closes #8

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,11 +4,16 @@ class SearchBar extends React.Component{
 
     state = {term: ''};
 
+    onFormSubmit(event){
+        event.preventDefault();
+        console.log(this.state.term)
+    }
+
 render(){
     return <div className="ui segment">
         Search Bar
         <br></br>
-        <form className="ui form">
+        <form className="ui form" onSubmit={(event)=>this.onFormSubmit(event)}>
         <input type='text' className="field" value={this.state.term} onChange={(e)=>this.setState({term:e.target.value})}></input>
         <button > Submit </button>
         </form>
