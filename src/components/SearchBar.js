2c0be5fd1c85d4598797b9e62e26225d536a0906
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-06-03 21:37:45 -0600

    alternate syntax

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,16 +2,12 @@ import React from 'react';
 
 class SearchBar extends React.Component{
 
-    onInputChange(event){
-        console.log(event.target.value)
-    }
-
 render(){
     return <div className="ui segment">
         Search Bar
         <br></br>
         <form className="ui form">
-        <input type='text' className="field" onChange={this.onInputChange}></input>
+        <input type='text' className="field" onChange={(e)=>{console.log(e.target.value)}}></input>
         <button > Submit </button>
         </form>
     </div>
