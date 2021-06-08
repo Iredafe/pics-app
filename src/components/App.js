db2d1505d755a14f42e248a94d76e00e46731718
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-06-07 21:39:37 -0600

    fetch data with axios closes #10

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import ImageList from './ImageList';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component{
 
   onSearchSubmit(term){
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+    params:{query: term},  
+    headers:{
+          Authorization: 'Client-ID m7F9R-ap0OPqyyyw0JFPJc1sz_9trOtqX4Oq0Fo_Uv8'
+          
+      }
+    });
   }
   render(){
     return(
