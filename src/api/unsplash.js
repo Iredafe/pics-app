41631ca3021d5bc9bb540a1a5ba742c29e72aca9
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-07-24 20:35:59 -0600

    update app

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
+    timeout:1000,
     headers:{
         Authorization: 'Client-ID m7F9R-ap0OPqyyyw0JFPJc1sz_9trOtqX4Oq0Fo_Uv8'
     }
